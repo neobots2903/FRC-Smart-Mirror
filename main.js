1c65b6514e701fe333a649979396a87f684f6bc4
Junha Lee <junhalee02@gmail.com>
2019-02-23 23:36:51 -0800

    Displays Date, Time, Logo, and Current Rank

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const canvas = document.getElementById("ctx");
 const c = canvas.getContext("2d");
-    
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth();
+let h = today.getHours();
+let m = today.getMinutes();
+
+let data = {
+    "teamRank": 0
+};
+
+const logo = new Image();
+logo.src = "logo.png";
+
+if (dd < 10) {
+    dd = "0"+dd;
+}
+
+if (mm < 10) {
+    mm = "0"+mm;
+}
+
 c.imageSmoothingEnabled = true;
 
 canvas.width = 1600;
@@ -13,37 +33,43 @@ function init() {
 
 init();
 
-let x = 0;
-
-let requestURL = "https://www.thebluealliance.com/api/v3/teams/1";
-let request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.setRequestHeader("X-TBA-Auth-Key", "0NiCsg5pJzCGOVmZTbYk0LdTZOXcDMIQJKThzoqIVBuEWSZ5dXbtTouAspaayL5B")
-request.send();
-
-request.onload = function() {
-    let information = request.response;
-    console.log("wowee");
-    console.log(information);
+function draw() {
+    today = new Date();
+    h = today.getHours();
+    m = today.getMinutes();
+    
+    c.save();
+    c.translate(0, canvas.height);
+    c.rotate(-(Math.PI/180)*90);
+    
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.height, canvas.width);
+    
+    c.textBaseline = "top";
+    c.font = "50px Segoe UI Light"
+    c.fillStyle = "white";
+    c.fillText(mm+"/"+dd, 30, 30);
+    c.fillText(h+":"+((m < 10)? m+"0":m), 740, 40);
+    c.fillText("#"+data["teamRank"], 30, 80);
+    
+    c.drawImage(logo, 300, 10, 300, 300);
+    
+    c.restore();
+    
+    window.requestAnimationFrame(draw);
 }
 
 
+let requestTeamStatus = "https://www.thebluealliance.com/api/v3/team/frc2903/event/2018wamou/status";
+let teamStatus = new XMLHttpRequest();
+teamStatus.open("GET", requestTeamStatus);
+teamStatus.responseType = "json";
+teamStatus.setRequestHeader("X-TBA-Auth-Key", "0NiCsg5pJzCGOVmZTbYk0LdTZOXcDMIQJKThzoqIVBuEWSZ5dXbtTouAspaayL5B")
+teamStatus.send();
 
-function draw() {
-    
-    
-    c.fillStyle = "black";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    
-    c.font = "150px cursive"
-    c.fillStyle = "red";
-    c.fillText("wowee", x++, 100);
-    
-    if (x == canvas.height) {
-        x= -100;
-    }
-    
-    window.requestAnimationFrame(draw);
+teamStatus.onload = function() {
+    let information = teamStatus.response;
+    console.log(information);
+    data["teamRank"] = information["qual"]["ranking"]["rank"];
 }
 
