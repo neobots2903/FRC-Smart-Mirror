d758f241b2dfe918d3464004893a6d4f5b815c37
TheRealJunhaLee <junhalee02@gmail.com>
2019-02-26 18:04:19 -0800

    Finished for the most part (still some things to be ironed out)

--- main.js
+++ main.js
@@ -19,15 +19,28 @@ let data = {
     "teamRank": 0,
     "latestRedPoints": 0,
     "latestBluePoints": 0,
-    "latestRedAlliance": [],
-    "latestBlueAlliance": [],
-    "topThreeNumbers": [],
-    "topThreeNames": [],
-    "topThreeRankingPoints": [],
-    "ourRankingPoints": 0
+    "latestRedAlliance": ["", "", ""],
+    "latestBlueAlliance": ["", "", ""],
+    "latestRedAllianceKeys": ["", "", ""],
+    "latestBlueAllianceKeys": ["", "", ""],
+    "latestRedNames": ["", "", ""],
+    "latestBlueNames": ["", "", ""],
+    "latestRedRanks": [0, 0, 0],
+    "latestBlueRanks": [0, 0, 0],
+    "latestRedRPoints": [0, 0, 0],
+    "latestBlueRPoints": [0, 0, 0],
+    "latestMatchNum": 0,
+    "topNumbers": [],
+    "topNames": [],
+    "topRankingPoints": [],
+    "teamRankingPoints": 0,
+    "teamWins": 0,
+    "teamLosses": 0,
+    "teamTies": 0
 };
 
-let teamInformation = {};
+let matchesUpdated = false;
+let eventsUpdated = false;
 
 //Loading the logo
 const logo = new Image();
@@ -71,7 +84,7 @@ function draw() {
     c.fillRect(0, 0, canvas.width, canvas.height);
     
     //Draw the logo
-    c.drawImage(logo, canvas.width/2-(350)/2, 00, 350, 350);
+   
     
     c.textAlign = "left";
     c.textBaseline = "top";
@@ -80,85 +93,124 @@ function draw() {
     
     //Stuff on left side of screen
     
+    
+    
+    
     //Month and day
     c.fillText(mm+"-"+dd, 30, 30);
     
+    c.font = "40px Segoe UI Light";
     c.textAlign = "center";
-    //TeamRank
-    c.fillText("Rank - "+data["teamRank"]+" (", 540, 330);
+    
+    c.save();
+    
+        c.translate(0, 40);
+    
+        c.drawImage(logo, canvas.width/2-(350)/2, 00, 350, 350);
+        c.fillText(`Rank - ${data["teamRank"]} (${data["teamRankingPoints"]} RP)`, 540, 340);
+        c.fillText(`${data["teamWins"]}W - ${data["teamTies"]}T - ${data["teamLosses"]}L`, 540, 400);
+
+    c.restore();
     
     c.textAlign="right";
     
     //Stuff on right side of screen
+    c.font = "60px Segoe UI Light";
     c.fillText(h+":"+((m < 10)? m+"0":m), 1050, 30);
     c.font = "35px Segoe UI Light";
     
-    c.fillText("Most Recent Match", 1050, 1100);
+    c.fillStyle = "white";
+    c.font = "40px Segoe UI Light";
+    
+    
+    for (let i in data["topNumbers"]) {
+        c.textAlign = "left";
+        c.fillText(`${parseInt(i)+1} - ${data["topNumbers"][i]}`, 30, 1140+55*i);
+        
+        c.textAlign = "center";
+        c.fillText(data["topNames"][i], 1080/2, 1140+55*i);
+        
+        c.textAlign = "right";
+        c.fillText(`${data["topRankingPoints"][i]} RP`, 1050, 1140+55*i);
+    }
+    
+    c.textAlign = "center";
+    c.fillText("Last", 1080/2, 1550);
+    c.fillText("Match", 1080/2, 1595);
+    
+    c.font = "45px Segoe UI Light";
+    c.fillText(data["latestMatchNum"], 1080/2, 1650);
     
     c.textAlign = "center";
     
+    c.font = "40px Segoe UI Light";
     c.fillStyle = "red";
-    c.fillText("Red", 660, 1140);
-    c.font = "45px Segoe Ui Light";
-    c.fillText(data["latestRedPoints"], 660, 1180);
+    c.fillText("Red Alliance", 200, 1450);
     
-    c.font = "20px Segoe Ui Light";
-    c.fillText(data["latestRedAlliance"].join("|"), 660, 1220);
+    c.font = "35px Segoe Ui Light";
+    c.fillText(`${data["latestRedPoints"]} Points`, 200, 1500);
+    c.fillRect(200-240/2, 1540, 240, 2);
     
-    c.font = "35px Segoe UI Light";
-    c.fillStyle = "cyan";
-    c.fillText("Blue", 800, 1140);
-    c.font = "45px Segoe UI Light";
-    c.fillText(data["latestBluePoints"], 800, 1180);
-    
-    c.font = "20px Segoe Ui Light";
-    c.fillText(data["latestBlueAlliance"].join("|"), 800, 1220);
-    
-    //c.font = "50px Segoe UI Light";
-    c.fillStyle = "white";
-    //c.fillText("Top Ranking Teams", 830, 260);
+    c.font = "30px Segoe Ui Light";
     
     c.textAlign = "left";
+    for (let i in data["latestRedAlliance"]) {
+        c.fillText(`${data["latestRedAlliance"][i]} - ${data["latestRedNames"][i]}`, 100, 1580+i*100);
+        c.fillText(`Rank ${data["latestRedRanks"][i]} - ${data["latestRedRPoints"][i]} RP`, 100, 1620+i*100);
+    }
     
+    c.textAlign = "center";
     c.font = "40px Segoe UI Light";
-    c.fillText("1. "+data["topThreeNumbers"][0], 650, 1340);
+    c.fillStyle = "cyan";
+    c.fillText("Blue Alliance", 1080-200, 1450);
     
-    c.font = "30px Segoe UI Light";
-    c.fillText(data["topThreeNames"][0]+"\t\t\t"+data["topThreeRankingPoints"][0]+" RP", 650, 1380);
+    c.font = "35px Segoe UI Light";
+    c.fillText(`${data["latestBluePoints"]} Points`, 1080-200, 1500);
+    c.fillRect(1080-200-240/2, 1540, 240, 2);
     
-    c.font = "40px Segoe UI Light";
-    c.fillText("2. "+data["topThreeNumbers"][1], 650, 1340+100);
+    c.font = "30px Segoe Ui Light";
     
-    c.font = "30px Segoe UI Light";
-    c.fillText(data["topThreeNames"][1]+"\t\t\t"+data["topThreeRankingPoints"][1]+" RP", 650, 1380+100);
+    c.textAlign = "right";
+    for (let i in data["latestBlueAlliance"]) {
+        c.fillText(`${data["latestBlueAlliance"][i]} - ${data["latestBlueNames"][i]}`, 1080-100, 1580+i*100);
+        c.fillText(`Rank ${data["latestBlueRanks"][i]} - ${data["latestBlueRPoints"][i]} RP`, 1080-100, 1620+i*100);
+    }
     
-    c.font = "40px Segoe UI Light";
-    c.fillText("3. "+data["topThreeNumbers"][2], 650, 1340+200);
-    
-    c.font = "30px Segoe UI Light";
-    c.fillText(data["topThreeNames"][2]+"\t\t\t"+data["topThreeRankingPoints"][2]+" RP", 650, 1380+200);
     
     //Restore transformations to default
     c.restore();
     
-    if (teamsFinishedLoading && eventsFinishedLoading) {
-        for (let i in teamInformation) {
-        if (topThreeKeys[0] == teamInformation[i]["key"]) {
-            topThreeNames[0] = teamInformation[i]["nickname"];
-        } else if (topThreeKeys[1] == teamInformation[i]["key"]) {
-            topThreeNames[1] = teamInformation[i]["nickname"];
-        } else if (topThreeKeys[2] == teamInformation[i]["key"]) {
-            topThreeNames[2] = teamInformation[i]["nickname"];
-        }
-        teamsFinishedLoading = false;
+    if (matchesLoaded && teamsLoaded && eventsLoaded) {
+        processMatches();
+        processMatchesAndTeams();
+        matchesLoaded = false;
+        matchesUpdated = true;
     }
     
-    data["topThreeNames"] = topThreeNames;
+    if (teamStatusLoaded) {
+        processTeamStatus();
+        teamStatusLoaded = false;
+    }
+    
+    if (teamsLoaded && eventsLoaded && !eventsProcessed) {
+        processEventRankings();
+        processRankingsAndTeams();
+        data["topNames"] = topNames;
+        eventsProcessed = true;
+        eventsUpdated = true;
+    }
+    
+    if (matchesUpdated && eventsUpdated) {
+        matchesUpdated = false;
+        eventsUpdated = false;
+        eventsLoaded = false;
     }
     
     //I don't actually know what this does to be honest, stack overflow just told me to put this here and it works
     window.requestAnimationFrame(draw);
 }
+
+
 
 //Request data pertaining to our team status
 let requestTeamStatus = `https://www.thebluealliance.com/api/v3/team/${teamKey}/event/${eventKey}/status`;
@@ -169,54 +221,127 @@ teamStatus.responseType = "json";
 teamStatus.setRequestHeader("X-TBA-Auth-Key", authKey);
 teamStatus.send();
 
+let teamStatusInfo;
+let teamStatusLoaded = false;
+
 teamStatus.onload = function() {
-    let information = teamStatus.response;
+    teamStatusInfo = teamStatus.response;
+    teamStatusLoaded = true;
     //console.log(information);
-    data["teamRank"] = information["qual"]["ranking"]["rank"];
 }
 
+function processTeamStatus() {
+    data["teamRank"] = teamStatusInfo["qual"]["ranking"]["rank"];
+    
+    data["teamWins"] = teamStatusInfo["qual"]["ranking"]["record"]["wins"];
+    data["teamLosses"] = teamStatusInfo["qual"]["ranking"]["record"]["losses"];
+    data["teamTies"] = teamStatusInfo["qual"]["ranking"]["record"]["ties"];
+}
+
+
 //Request data pertaining to matches our robot was involved in
-let requestTeamMatches = `https://www.thebluealliance.com/api/v3/team/${teamKey}/event/${eventKey}/matches`;
-let teamMatches = new XMLHttpRequest();
+let requestMatches = `https://www.thebluealliance.com/api/v3/event/${eventKey}/matches`;
+let matches = new XMLHttpRequest();
 
-teamMatches.open("GET", requestTeamMatches);
-teamMatches.responseType = "json";
-teamMatches.setRequestHeader("X-TBA-Auth-Key", authKey);
-teamMatches.send();
+matches.open("GET", requestMatches);
+matches.responseType = "json";
+matches.setRequestHeader("X-TBA-Auth-Key", authKey);
+matches.send();
 
-teamMatches.onload = function() {
-    let information = teamMatches.response;
-    //console.log(information);
-    
-    
-    //Modify [0] so that it actually grabs the latest match
+let matchesInfo;
+let matchesLoaded = false;
+
+matches.onload = function() {
+    matchesInfo = matches.response;
+    matchesLoaded = true;
+}
+
+function processMatches() {
     
     let latestTime = 0;
     let latestTimeKey = 0;
-    for (let i in information) {
-        if (information[i]["actual_time"] > latestTime) {
+    for (let i in matchesInfo) {
+        if (matchesInfo[i]["actual_time"] > latestTime && matchesInfo[i]["comp_level"] == "qm") {
             latestTimeKey = i;
-            latestTime = information[i]["actual_time"];
+            latestTime = matchesInfo[i]["actual_time"];
         }
     }
     
-    data["latestRedPoints"] = information[latestTimeKey]["alliances"]["red"]["score"];
-    data["latestBluePoints"] = information[latestTimeKey]["alliances"]["blue"]["score"];
+    data["latestRedPoints"] = matchesInfo[latestTimeKey]["alliances"]["red"]["score"];
+    data["latestBluePoints"] = matchesInfo[latestTimeKey]["alliances"]["blue"]["score"];
     
-    let redTeamKeys = information[latestTimeKey]["alliances"]["red"]["team_keys"];
+    let redTeamKeys = matchesInfo[latestTimeKey]["alliances"]["red"]["team_keys"];
+    let redTeamNumbers = [];
     for (let i in redTeamKeys) {
-        redTeamKeys[i] = redTeamKeys[i].slice(3, redTeamKeys[i].length);
+        redTeamNumbers.push(redTeamKeys[i].slice(3, redTeamKeys[i].length));
     }
     
-    data["latestRedAlliance"] = redTeamKeys;
+    data["latestRedAlliance"] = redTeamNumbers;
+    data["latestRedAllianceKeys"] = redTeamKeys;
     
-    let blueTeamKeys = information[latestTimeKey]["alliances"]["blue"]["team_keys"];
+    let blueTeamKeys = matchesInfo[latestTimeKey]["alliances"]["blue"]["team_keys"];
+    let blueTeamNumbers = [];
     for (let i in blueTeamKeys) {
-        blueTeamKeys[i] = blueTeamKeys[i].slice(3, blueTeamKeys[i].length);
+        blueTeamNumbers.push(blueTeamKeys[i].slice(3, blueTeamKeys[i].length));
     }
     
-    data["latestBlueAlliance"] = blueTeamKeys;
+    data["latestBlueAlliance"] = blueTeamNumbers;
+    data["latestBlueAllianceKeys"] = blueTeamKeys
+    
+    data["latestMatchNum"] = matchesInfo[latestTimeKey]["match_number"];
+    
+    
+    
 }
+
+function processMatchesAndTeams() {
+    
+    
+    for (let i in teamInfo) {
+        if (data["latestRedAllianceKeys"][0] == teamInfo[i]["key"]) {
+            data["latestRedNames"][0] = teamInfo[i]["nickname"];
+        } else if (data["latestRedAllianceKeys"][1] == teamInfo[i]["key"]) {
+            data["latestRedNames"][1] = teamInfo[i]["nickname"];
+        } else if (data["latestRedAllianceKeys"][2] == teamInfo[i]["key"]) {
+            data["latestRedNames"][2] = teamInfo[i]["nickname"];
+        }
+        
+        if (data["latestBlueAllianceKeys"][0] == teamInfo[i]["key"]) {
+            data["latestBlueNames"][0] = teamInfo[i]["nickname"];
+        } else if (data["latestBlueAllianceKeys"][1] == teamInfo[i]["key"]) {
+            data["latestBlueNames"][1] = teamInfo[i]["nickname"];
+        } else if (data["latestBlueAllianceKeys"][2] == teamInfo[i]["key"]) {
+            data["latestBlueNames"][2] = teamInfo[i]["nickname"];
+        }
+    }
+    
+    for (let i in eventsInfo["rankings"]) {
+        if (eventsInfo["rankings"][i]["team_key"] == data["latestRedAllianceKeys"][0]) {
+            data["latestRedRanks"][0] = eventsInfo["rankings"][i]["rank"];
+            data["latestRedRPoints"][0] = eventsInfo["rankings"][i]["extra_stats"][0];
+        } else if (eventsInfo["rankings"][i]["team_key"] == data["latestRedAllianceKeys"][1]) {
+            data["latestRedRanks"][1] = eventsInfo["rankings"][i]["rank"];
+            data["latestRedRPoints"][1] = eventsInfo["rankings"][i]["extra_stats"][0];
+        } else if (eventsInfo["rankings"][i]["team_key"] == data["latestRedAllianceKeys"][2]) {
+            data["latestRedRanks"][2] = eventsInfo["rankings"][i]["rank"];
+            data["latestRedRPoints"][2] = eventsInfo["rankings"][i]["extra_stats"][0];
+        }
+        
+        if (eventsInfo["rankings"][i]["team_key"] == data["latestBlueAllianceKeys"][0]) {
+            data["latestBlueRanks"][0] = eventsInfo["rankings"][i]["rank"];
+            data["latestBlueRPoints"][0] = eventsInfo["rankings"][i]["extra_stats"][0];
+        } else if (eventsInfo["rankings"][i]["team_key"] == data["latestBlueAllianceKeys"][1]) {
+            data["latestBlueRanks"][1] = eventsInfo["rankings"][i]["rank"];
+            data["latestBlueRPoints"][1] = eventsInfo["rankings"][i]["extra_stats"][0];
+        } else if (eventsInfo["rankings"][i]["team_key"] == data["latestBlueAllianceKeys"][2]) {
+            data["latestBlueRanks"][2] = eventsInfo["rankings"][i]["rank"];
+            data["latestBlueRPoints"][2] = eventsInfo["rankings"][i]["extra_stats"][0];
+        }
+    }
+    
+}
+
+
 
 //Request data pertaining to teams at the event
 let requestTeams = `https://www.thebluealliance.com/api/v3/event/${eventKey}/teams`;
@@ -227,13 +352,19 @@ teams.responseType = "json";
 teams.setRequestHeader("X-TBA-Auth-Key", authKey);
 teams.send();
 
+let teamsLoaded = false;
+let teamInfo;
 
-let teamsFinishedLoading = false;
 teams.onload = function() {
-    let information = teams.response;
-    teamInformation = information;
-    teamsFinishedLoading = true;
+    teamInfo = teams.response;
+    teamsLoaded = true;
 }
+
+function processTeamInfo() {
+    
+}
+
+
 
 //Request data pertaining to all teams attending the event
 let requestEventRankings = `https://www.thebluealliance.com/api/v3/event/${eventKey}/rankings`;
@@ -244,35 +375,76 @@ eventRankings.responseType = "json";
 eventRankings.setRequestHeader("X-TBA-Auth-Key", authKey);
 eventRankings.send();
 
-let topThreeKeys = [];
-let topThreeNames = [];
+let topKeys = [];
+let topNames = [];
 
-let eventsFinishedLoading = false;
+let eventsLoaded = false;
+let eventsInfo;
+let eventsProcessed = false;
+
 eventRankings.onload = function() {
     let information = eventRankings.response;
-    console.log(information);
-    
-    data["topThreeNumbers"] = [information["rankings"][0]["team_key"].slice(3, information["rankings"][0]["team_key"].length),
-                               information["rankings"][1]["team_key"].slice(3, information["rankings"][1]["team_key"].length),
-                               information["rankings"][2]["team_key"].slice(3, information["rankings"][2]["team_key"].length)
-                              ];
-    
-    
-    data["topThreeRankingPoints"] = [information["rankings"][0]["extra_stats"][0],
-                                     information["rankings"][1]["extra_stats"][0],
-                                     information["rankings"][2]["extra_stats"][0]
-                                    ];
-    
-    topThreeKeys = [information["rankings"][0]["team_key"],
-                        information["rankings"][1]["team_key"],
-                        information["rankings"][2]["team_key"]
-                       ];
-    
-    topThreeNames = ["", "", ""];
-    
-    console.log(teamInformation);
-    
-    eventsFinishedLoading = true;
+    eventsInfo = eventRankings.response;
+    eventsLoaded = true;
 }
 
+function processEventRankings() {
+    
+    for (var i  = 0 ; i < 5 ; i++) {
+        data["topNumbers"].push(eventsInfo["rankings"][i]["team_key"].slice(3, eventsInfo["rankings"][0]["team_key"].length));
+        data["topRankingPoints"].push(eventsInfo["rankings"][i]["extra_stats"][0]);
+        topKeys.push(eventsInfo["rankings"][i]["team_key"]);
+    }
+    
+    let ourTeamKey = 0;
+    for (let i in eventsInfo["rankings"]) {
+        if (eventsInfo["rankings"][i]["team_key"] == "frc2903") {
+            ourTeamKey = i;
+            break;
+        }
+    }
+    
+    data["teamRankingPoints"] = eventsInfo["rankings"][ourTeamKey]["extra_stats"][0];
+    
+}
 
+function processRankingsAndTeams() {
+    for (let i in teamInfo) {
+        if (topKeys[0] == teamInfo[i]["key"]) {
+            topNames[0] = teamInfo[i]["nickname"];
+        } else if (topKeys[1] == teamInfo[i]["key"]) {
+            topNames[1] = teamInfo[i]["nickname"];
+        } else if (topKeys[2] == teamInfo[i]["key"]) {
+            topNames[2] = teamInfo[i]["nickname"];
+        } else if (topKeys[3] == teamInfo[i]["key"]) {
+            topNames[3] = teamInfo[i]["nickname"];
+        } else if (topKeys[4] == teamInfo[i]["key"]) {
+            topNames[4] = teamInfo[i]["nickname"];
+        }
+    }
+    
+}
+
+window.setInterval(() => {
+    teamStatus.open("GET", requestTeamStatus);
+    teamStatus.responseType = "json";
+    teamStatus.setRequestHeader("X-TBA-Auth-Key", authKey);
+    teamStatus.send();
+    
+    matches.open("GET", requestMatches);
+    matches.responseType = "json";
+    matches.setRequestHeader("X-TBA-Auth-Key", authKey);
+    matches.send();
+    
+    teams.open("GET", requestTeams);
+    teams.responseType = "json";
+    teams.setRequestHeader("X-TBA-Auth-Key", authKey);
+    teams.send();
+    
+    eventRankings.open("GET", requestEventRankings);
+    eventRankings.responseType = "json";
+    eventRankings.setRequestHeader("X-TBA-Auth-Key", authKey);
+    eventRankings.send();
+    
+    console.log("data updated");
+}, 300000)
